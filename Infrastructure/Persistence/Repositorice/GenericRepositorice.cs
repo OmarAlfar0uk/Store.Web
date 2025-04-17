3ea54e487061c1c41fb9154bf0a02acbf43aec02
omaralfarouk646@gmail.com <omaralfarouk646@gmail.com>
2025-04-18 00:20:07 +0200

    Generic Repository

---
+++ Infrastructure/Persistence/Repositorice/GenericRepositorice.cs
@@ -0,0 +1,28 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositorice
+{
+    public class GenericRepositorice<TEntity, Tkey>(StoreDbContext _dbContext) : IGenericRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        public async Task AddAsync(TEntity entity) => await _dbContext.Set<TEntity>().AddAsync(entity);
+      
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()=> await _dbContext.Set<TEntity>().ToListAsync();
+     
+
+        public async Task<TEntity?> GetByIdAsync(Tkey Id) => await _dbContext.Set<TEntity>().FindAsync(Id); 
+       
+
+        public void Remove(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity); 
+
+        public void Update(TEntity entity)=> _dbContext.Set<TEntity>().Update(entity);
+    }
+}
