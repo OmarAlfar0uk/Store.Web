a03866835bfe5a08be2d14fedf38fe9592a3837d
omaralfarouk646@gmail.com <omaralfarouk646@gmail.com>
2025-04-24 17:29:52 +0200

    finsh

---
+++ Infrastructure/Presentaion/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObject.BasketModuleDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentaion.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketController(IServiceManger _serviceManger) : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<BasketDto>> GetBasket(string Key)
+        {
+            var Basket =await _serviceManger.BasketService.GetBasketAsync(Key);
+            return Ok(Basket);  
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<BasketDto>> CreateOrUpdateBasket(BasketDto Basket) 
+        {
+            var basket =await _serviceManger.BasketService.CreateOrUpdateBasketAsync(Basket);
+            return Ok(basket);
+        }
+
+        [HttpDelete("{Key}")]
+        public async Task<ActionResult<bool>> DeleteBasket(string Key)
+        { 
+            var Result =await _serviceManger.BasketService.DeleteBasketAsync(Key);
+            return Ok(Result);
+        }
+    }
+}
